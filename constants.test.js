a943afb7988256c0627e9dfc3e5c6bb0694a2ea1
Brenton <brenton.j.andrews@gmail.com>
2023-05-04 11:38:02 -0600

    Tetromino rotation tests successfully, counterclockwise rotation not needed per specification.

--- constants.test.js
+++ constants.test.js
@@ -1,4 +1,5 @@
-// Testing suite for tetromine behavior outside of game impelementation.
+// Testing for tetromine behavior specified at https://tetris.fandom.com/wiki/SRS.
+
 const TETROMINOS = {
   'I':[
     [0,0,0,0],
@@ -44,18 +45,90 @@ const TETROMINOS = {
 }
 
 const CLOCKWISE_90_DEG = {
+  'I':[
+    [0,0,1,0],
+    [0,0,1,0],
+    [0,0,1,0],
+    [0,0,1,0]
+  ],
+
   'J':[
     [0,1,1],
     [0,1,0],
+    [0,1,0]
+  ],
+
+  'L':[
+    [0,1,0],
+    [0,1,0],
+    [0,1,1]
+  ],
+
+  'O':[
+    [1,1],
+    [1,1]
+  ],
+
+  'S':[
+    [0,1,0],
+    [0,1,1],
+    [0,0,1]
+  ],
+
+  'T':[
+    [0,1,0],
+    [0,1,1],
+    [0,1,0]
+  ],
+
+  'Z':[
+    [0,0,1],
+    [0,1,1],
     [0,1,0]
   ]
 }
 
 const COUNTERCLOCKWISE_90_DEG = {
+  'I':[
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0]
+  ],
+
   'J':[
     [0,1,0],
     [0,1,0],
     [1,1,0]
+  ],
+
+  'L':[
+    [0,0,1],
+    [1,1,1],
+    [0,0,0]
+  ],
+
+  'O':[
+    [1,1],
+    [1,1]
+  ],
+
+  'S':[
+    [0,1,1],
+    [1,1,0],
+    [0,0,0]
+  ],
+
+  'T':[
+    [0,1,0],
+    [1,1,1],
+    [0,0,0]
+  ],
+
+  'Z':[
+    [1,1,0],
+    [0,1,1],
+    [0,0,0]
   ]
 }
 
@@ -64,10 +137,8 @@ function rotateMatrix90DegClockWise (matrix) {
 
   const result = matrix.map((row, i) =>
     row.map((val, j) => matrix[N - j][i])
-    
   );
 
-  console.log(result);
   return result;
 }
 
@@ -75,7 +146,7 @@ function rotateMatrix90DegCounterClockWise (matrix) {
   const N = matrix.length - 1;
 
   const result = matrix.map((row, i) =>
-    row.map((val, j) => matrix[i][N - j])
+    row.map((val, j) => matrix[j][N - i])
   );
 
   console.log(result);
@@ -90,14 +161,25 @@ function rotateMatrix360DegClockWise (matrix) {
   return result;
 }
 
-test('Rotate tetromino 90 degrees clockwise', () => {
+test('Rotate all tetrominos 90 degrees clockwise', () => {
   expect(rotateMatrix90DegClockWise(TETROMINOS.J)).toStrictEqual(CLOCKWISE_90_DEG.J);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.L)).toStrictEqual(CLOCKWISE_90_DEG.L);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.I)).toStrictEqual(CLOCKWISE_90_DEG.I);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.L)).toStrictEqual(CLOCKWISE_90_DEG.L);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.O)).toStrictEqual(CLOCKWISE_90_DEG.O);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.S)).toStrictEqual(CLOCKWISE_90_DEG.S);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.T)).toStrictEqual(CLOCKWISE_90_DEG.T);
+  expect(rotateMatrix90DegClockWise(TETROMINOS.Z)).toStrictEqual(CLOCKWISE_90_DEG.Z);
 });
 
-// test('Rotate tetromino 360 degrees clockwise.', () => {
-//   expect(rotateMatrix360DegClockWise(TETROMINOS.J)).toStrictEqual(TETROMINOS.J);
-// });
+test('Rotate all tetrominos 360 degrees clockwise.', () => {
+  expect(rotateMatrix360DegClockWise(TETROMINOS.J)).toStrictEqual(TETROMINOS.J);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.L)).toStrictEqual(TETROMINOS.L);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.I)).toStrictEqual(TETROMINOS.I);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.L)).toStrictEqual(TETROMINOS.L);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.O)).toStrictEqual(TETROMINOS.O);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.S)).toStrictEqual(TETROMINOS.S);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.T)).toStrictEqual(TETROMINOS.T);
+  expect(rotateMatrix360DegClockWise(TETROMINOS.Z)).toStrictEqual(TETROMINOS.Z);
+});
 
-test('Rotate tetromino 90 degrees counterclockwise', () => {
-  expect(rotateMatrix90DegCounterClockWise(TETROMINOS.J)).toStrictEqual(COUNTERCLOCKWISE_90_DEG.J);
-})
